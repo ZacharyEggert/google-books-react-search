a18ae751de53f4532707fedc60931503944df416
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-07-03 17:18:58 -0700

    working connected to mongo Atlas

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 const path = require('path');
+
 const express = require('express');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+
 const routes = require('./routes');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000
 
+const store = new MongoDBStore({
+    uri: process.env.MONGO_SESSION_URI || 'mongodb://localhost:27017/Books',
+    collection:'sessions'
+}, (error) => {error?console.error({error}):null;})
 
+store.on('error', (error) => {
+    console.error({error});
+})
 
 const app = express();
 
 const sess = {
     secret : process.env.SECRET || 'secret_string',
     cookie: {},
+    store,
     resave: false,
     saveUninitialized: false,
 }
@@ -26,6 +37,7 @@ app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "client", "build", "index.html"));
     }catch(err){
         console.error(err);
+        res.status(500).json(err);
     }
 });
 
