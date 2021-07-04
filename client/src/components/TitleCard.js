3eab4e4df2ac97e4b3998f3edd67e43f42e7b2d6
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-07-03 22:36:11 -0700

    title card

---
+++ client/src/components/TitleCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TitleCard = () => {
+
+    return (
+        <div className='w-full py-4 my-4 border-2 border-black border-solid'>
+            <h1 className='text-2xl text-center sm:text-4xl md:text-5xl g-font'>(React)<br className='hidden'/> Google Books Search</h1>
+            <h3 className='py-4 text-lg text-center sm:text-xl md:text-2xl'>Search for and Save Books of Interest</h3>
+        </div>
+    )
+}
+
+export default TitleCard;
