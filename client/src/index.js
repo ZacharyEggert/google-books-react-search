612a4dc98d6c7b2131630e764ca4814bd8fd91f8
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-07-04 20:45:05 -0700

    add dotenv for API keys

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,8 @@ import ReactDOM from "react-dom";
 import './index.css';
 
 import App from "./App";
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 ReactDOM.render(<App />, document.getElementById("root"));
